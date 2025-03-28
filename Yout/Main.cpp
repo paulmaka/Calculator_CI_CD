66e158da5707f4d2a36ab713ec66b61bf0f11a00
Павел Макаренко <mpa.ryazan@gmail.com>
2025-03-28 19:53:59 +0300

    srever variant

--- Yout/Main.cpp
+++ Yout/Main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include "httplib.h"
 #include "Math.h"
 
 using namespace std;
@@ -6,7 +7,33 @@ using namespace std;
 
 //hello
 
+int main() {
+	httplib::Server svr;
 
+	svr.Get(R"(/calc/(\d+)/([\+\-\*/\^])/(\d+))", [](const httplib::Request& req, httplib::Response& res) {
+		int a = std::stoi(req.matches[1]);
+		std::string op = req.matches[2];
+		int b = std::stoi(req.matches[3]);
+		int result = 0;
+
+		if (op == "+") result = sum(a, b);
+		else if (op == "-") result = sub(a, b);
+		else if (op == "*") result = mul(a, b);
+		else if (op == "/") result = division(a, b);
+		else if (op == "^") result = powe(a, b);
+		else {
+			res.status = 400;
+			res.set_content("Я не знаю такой операции, вернитесь, когда меня обновят...", "text/plain");
+			return;
+		}
+
+		res.set_content(std::to_string(result), "text/plain");
+		});
+
+	svr.listen("0.0.0.0", 8080);
+}
+
+/*
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -42,4 +69,4 @@ int main()
 	
 }
 
-
+*/
